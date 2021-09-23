bea8d4c200519f700da80d3574ad89c9a05b63d7
Scott Burton <burtons@byui.edu>
2021-09-23 08:56:44 -0600

    Initial Version

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cse210_student_csharp_dice_complete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director theDirector = new Director();
+            theDirector.StartGame();
+        }
+    }
+}
